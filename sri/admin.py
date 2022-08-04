1b36630e36b07877794af52a82f32f49f09e4262
Junior Mota <motaibhaltair@gmail.com>
2022-08-04 15:51:34 -0500

    Primer commiit del proyecto

---
+++ sri/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Empresa, DatosEmpresa
+# Register your models here.
+
+admin.site.register(Empresa)
+admin.site.register(DatosEmpresa)
---
+++ sri/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.nombre
+
+class DatosEmpresa(models.Model):
+    ruc = models.CharField(max_length=20)
+    clave = models.CharField(max_length=100)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.empresa
